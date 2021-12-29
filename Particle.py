567d7d3bada25bc72f89a9d84a4be12320e64e11
Mateusz Roszkowski <blackspider1@o2.pl>
2021-12-29 20:03:07 +0100

    Full algorithm implemented
    Co-authored-by: Hanna-J-K <Hanna-J-K@users.noreply.github.com>

---
+++ Particle.py
@@ -0,0 +1,83 @@
+import numpy as np
+import math
+
+INERTIA_WEIGHT = 0.5
+COGNITIVE_CONSTANT = 1
+SOCIAL_CONSTANT = 2
+
+
+def FUNCTION(x, y):
+    return -(y + 47) * math.sin(math.sqrt(abs(x / 2 + (y + 47)))) - x * math.sin(math.sqrt(abs(x - (y + 47))))
+# placeholder jak cos
+
+
+global_best_x = 0
+global_best_y = 0
+
+rng = np.random.default_rng()
+
+
+class Particle():
+    def __init__(self, bounds_x: tuple, bounds_y):
+        self.velocity_x = []
+        self.velocity_y = []
+        self.x = []
+        self.y = []
+        self.adaptation = []
+        self.best_x = 0
+        self.best_y = 0
+        self.best_adaptation = np.inf
+        self.bounds_x = bounds_x
+        self.bounds_y = bounds_y
+
+        self.velocity_x.append(0)
+        self.velocity_y.append(0)
+        self.x.append(rng.uniform(bounds_x[0], bounds_x[1]))
+        self.y.append(rng.uniform(bounds_y[0], bounds_y[1]))
+
+    def calculate_inertia(self):
+        inertia_x = INERTIA_WEIGHT * self.velocity_x[-1]
+        inertia_y = INERTIA_WEIGHT * self.velocity_y[-1]
+        return inertia_x, inertia_y
+
+    def calculate_cognitive_components(self):
+        cognitive_x = (COGNITIVE_CONSTANT * rng.random()) * \
+            (self.best_x - self.x[-1])
+        cognitive_y = (COGNITIVE_CONSTANT * rng.random()) * \
+            (self.best_y - self.y[-1])
+        return cognitive_x, cognitive_y
+
+    def calculate_social_components(self):
+        social_x = (SOCIAL_CONSTANT * rng.random()) * \
+            (global_best_x - self.x[-1])
+        social_y = (SOCIAL_CONSTANT * rng.random()) * \
+            (global_best_y - self.y[-1])
+        return social_x, social_y
+
+    def calculate_new_velocity(self):
+        inertia_x, inertia_y = self.calculate_inertia()
+        cognitive_x, cognitive_y = self.calculate_cognitive_components()
+        social_x, social_y = self.calculate_social_components()
+        self.velocity_x.append(inertia_x + cognitive_x + social_x)
+        self.velocity_y.append(inertia_y + cognitive_y + social_y)
+
+    def move_to_new_position(self):
+        self.x[-1] += self.velocity_x[-1]
+        self.y[-1] += self.velocity_y[-1]
+
+        if self.x[-1] < self.bounds_x[0]:
+            self.x[-1] = self.bounds_x[0]
+        if self.x[-1] > self.bounds_x[1]:
+            self.x[-1] = self.bounds_x[1]
+
+        if self.y[-1] < self.bounds_y[0]:
+            self.y[-1] = self.bounds_y[0]
+        if self.y[-1] > self.bounds_y[1]:
+            self.y[-1] = self.bounds_y[1]
+
+    def find_best_adaptation(self):
+        self.adaptation.append(FUNCTION(self.x[-1], self.y[-1]))
+        if self.adaptation[-1] < self.best_adaptation:
+            self.best_x = self.x[-1]
+            self.best_y = self.y[-1]
+            self.best_adaptation = self.adaptation[-1]
---
+++ Swarm.py
@@ -0,0 +1,39 @@
+import numpy as np
+from Particle import Particle
+import matplotlib.pyplot as plt
+
+POPULATION = 90
+ITERATIONS = 500
+
+
+def generate_swarm(bounds_x, bounds_y):
+    return [Particle(bounds_x, bounds_y) for _ in range(POPULATION)]
+
+
+def find_minimum(swarm):
+    global_best_adaptation = np.inf
+    best_positions_x = []
+    best_positions_y = []
+    best_adaptations = []
+    for _ in range(ITERATIONS):
+        for particle in swarm:
+            particle.find_best_adaptation()
+            particle.calculate_new_velocity()
+            particle.move_to_new_position()
+
+            if particle.best_adaptation < global_best_adaptation:
+                best_positions_x.append(particle.best_x)
+                best_positions_y.append(particle.best_y)
+                global_best_adaptation = particle.best_adaptation
+        best_adaptations.append(global_best_adaptation)
+
+    return best_positions_x, best_positions_y, best_adaptations
+
+
+if __name__ == '__main__':
+    swarm = generate_swarm((-512, 512), (-512, 512))
+    best_positions_x, best_positions_y, best_adaptations = find_minimum(swarm)
+    print(best_positions_x)
+    print(best_positions_y)
+    plt.plot(np.arange(ITERATIONS), best_adaptations)
+    plt.show()
